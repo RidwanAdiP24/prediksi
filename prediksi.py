b809d75f11dc32db5e78ad408d6d924b16c15f46
RidwanAdiP24 <93441895+RidwanAdiP24@users.noreply.github.com>
2023-06-15 20:54:38 +0700

    Update prediksi.py

--- prediksi.py
+++ prediksi.py
@@ -1,10 +1,9 @@
 import streamlit as st
+# Import library
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
 
+# Import dataset
 dataset = pd.read_csv('Data_Skripsi_2023.csv', sep=";")
 print(dataset)
 
@@ -68,29 +67,29 @@ print (accuracies.mean()*100)
 
 # Dictionary mapping team codes to team name
 team_mapping = {
-    1: 'Udinese',
-    2: 'Milan',
+    1: 'Roma',
+    2: 'Inter',
     3: 'Benevento',
-    4: 'Napoli',
+    4: 'Spal',
     5: 'Genoa',
     6: 'Crotone',
-    7: 'Fiorentina',
-    8: 'Lazio',
-    9: 'Inter',
+    7: 'Napoli',
+    8: 'Fiorentina',
+    9: 'Chievo',
     10: 'Sampdoria',
     11: 'Bologna',
     12: 'Cagliari',
-    13: 'Roma',
+    13: 'Udinese',
     14: 'Sasauolo',
     15: 'Verona',
     16: 'Torino',
-    17: 'Spal',
-    18: 'Juventus',
+    17: 'Lazio',
+    18: 'Atalanta',
     19: 'Parma',
     20: 'Lecce',
     21: 'Empoli',
-    22: 'Atalanta',
-    23: 'Chievo',
+    22: 'Juventus',
+    23: 'Milan',
     24: 'Frosinone',
     25: 'Brescia',
     26: 'Spezia',
@@ -98,27 +97,44 @@ team_mapping = {
     28: 'Venezia',
 }
 
-# Membuat dictionary untuk mapping hasil prediksi
-result_mapping = {
-    0: "Draw",
-    1: "Home Win",
-    2: "Away Win"
-}
-
-# Mengganti hasil prediksi menggunakan mapping
-y_pred_labels = [result_mapping[pred] for pred in y_pred]
-
-# Menampilkan hasil prediksi
-print(y_pred_labels)
-
-
 home_team = dataset.iloc[X_test[:, 0], 0].map(team_mapping).values
 away_team = dataset.iloc[X_test[:, 1], 1].map(team_mapping).values
-result = np.column_stack((home_team, away_team, y_pred_labels))
+result = np.column_stack((home_team, away_team, y_pred))
 
 df_result = pd.DataFrame(result, columns=['Home Team', 'Away Team', 'Result'])
 print(df_result)
 
+from IPython.display import display
+display(df_result)
+
+# Kalkulasi final point by team
+points = {}
+for i in range(len(result)):
+    home = result[i][0]
+    away = result[i][1]
+    pred = result[i][2]
+    if home not in points:
+        points[home] = 0
+    if away not in points:
+        points[away] = 0
+    if pred == 0:
+        points[home] += 0
+        points[away] += 3
+    elif pred == 1:
+        points[home] += 1
+        points[away] += 1
+    else:
+        points[home] += 3
+        points[away] += 0
+
+# Sorting teams by points
+sorted_points = sorted(points.items(), key=lambda x: x[1], reverse=True)
+
+# Print final standings
+print("Final Standings:")
+print("Team\tPoints")
+for i in range(len(sorted_points)):
+    print(f"{sorted_points[i][0]}\t{sorted_points[i][1]}")
 
 # Define Streamlit app
 def main():
